ab5aec5dfee44a43a4bc75cd86fae325a9159d11
Marinescu <212517530@logon.ds.ge.com>
2019-02-13 11:34:45 +0200

    Add project files.

---
+++ L1/pch.cpp
@@ -0,0 +1,11 @@
+// pch.cpp: source file corresponding to pre-compiled header; necessary for compilation to succeed
+
+#include <iostream>
+#include "pch.h"
+
+void myprint()
+{
+	std::cout << "Printing!\n";
+}
+
+// In general, ignore this file, but keep it around if you are using pre-compiled headers.
